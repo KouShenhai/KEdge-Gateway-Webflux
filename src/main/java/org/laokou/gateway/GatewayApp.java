716e7bedcb371b966cba5c9c7bda62836e89d425
KouShenhai <2413176044@qq.com>
2026-09-06 20:23:25 +0800

    feat: 提交代码

---
+++ src/main/java/org/laokou/gateway/GatewayApp.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2022-2026 KEdge-Gateway-Webflux Author or Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.laokou.gateway;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.util.StopWatch;
+
+/**
+ *
+ * @author laokou
+ */
+@EnableConfigurationProperties
+@SpringBootApplication(scanBasePackages = "org.laokou.gateway")
+class GatewayApp {
+
+	// @formatter:off
+	static void main(String[] args) {
+		StopWatch stopWatch = new StopWatch("边缘网关应用程序");
+		stopWatch.start();
+		new SpringApplicationBuilder(GatewayApp.class).web(WebApplicationType.REACTIVE).run(args);
+		stopWatch.stop();
+	}
+	// @formatter:on
+
+}
